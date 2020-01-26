bc748045dd571d7f17e55a6d9069f0d644e366e0
cajallen <cajallen427@gmail.com>
2020-01-26 12:23:36 -0600

    frontpage update

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import Customers from './components/customers';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import Navbar from './components/Navbar';
+import { Typography } from '@material-ui/core';
+import { Container}  from '@material-ui/core';
+import { Divider } from '@material-ui/core';
 
 
 class App extends Component {
@@ -35,9 +38,23 @@ class App extends Component {
       <div className="App">
         {/* <Navbar signedIn={this.state.signedIn} signOut={this.signOut} signIn={this.signIn} /> */}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">GoHelpMe - Empowering the local communities across the world</h1>
         </header>
+        <Container maxWidth="sm" style={{padding:"20px"}}>
+          <Typography variant="h5" align="left">
+            Header
+          </Typography>
+          <Typography align="left">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </Typography>
+          <Divider />
+          <Typography variant="h5" align="left">
+            Header
+          </Typography>
+          <Typography align="left">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+          </Typography>
+        </Container>
       </div>
     );
   }
