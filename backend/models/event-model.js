21bf714fe5509c3a0a49c9f482b4c6fd2bbf2be5
klforthwind <kl.forthwind@gmail.com>
2020-01-26 00:55:20 -0600

    add more fields to event

--- backend/models/event-model.js
+++ backend/models/event-model.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose')
 const CoordinateSchema = require('./coordinate-model').CoordinateSchema
+const UserSchema = require('./user-model').UserSchema
 const Schema = mongoose.Schema
 
 const EventSchema = new Schema(
     {
         name : {type: String, required: true},
         location : {type: CoordinateSchema, required: true},
-        info : {type: String, required: true}
+        info : {type: String, required: true},
+        creator : {type: UserSchema, required:true},
+        volunteers : {type: [UserSchema], default: []},
+        target_vol_count : {type: Number, required:true},
+        current_vol_count : {type: Number, required:true}
     },
     { timestamps : true}
 )
