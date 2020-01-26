908ff461611bfa8e87db0f4bf0b5896c80746a30
klforthwind <kl.forthwind@gmail.com>
2020-01-26 04:21:57 -0600

    finish up backend

--- backend/routes/event-router.js
+++ backend/routes/event-router.js
@@ -6,9 +6,10 @@ router.post('/create', EventCtrl.createEvent)
 router.put('/update/:id', EventCtrl.updateEvent)
 router.get('/events', EventCtrl.getEvents)
 router.get('/event/:id', EventCtrl.getEventByID)
-router.get('/event/creator/:owner', EventCtrl.getEventsByOwner)
+router.get('/event/creator/:creator', EventCtrl.getEventsByCreator)
 router.get('/event/volunteer/:volunteer', EventCtrl.getEventsByVolunteer)
 router.put('/add_volunteer/:id', EventCtrl.addVolunteerToEvent)
 router.put('/remove_volunteer/:id', EventCtrl.removeVolunteerFromEvent)
+router.delete('/delete/:id', EventCtrl.deleteEvent)
 
 module.exports = router
