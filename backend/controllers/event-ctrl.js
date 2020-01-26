38379ecc78c7f865db260f633e471873c0792efe
klforthwind <kl.forthwind@gmail.com>
2020-01-26 01:20:49 -0600

    add more controller function

--- backend/controllers/event-ctrl.js
+++ backend/controllers/event-ctrl.js
@@ -73,28 +73,64 @@ deleteEvent = async (req, res) => {
 }
 
 getEvents = async (req, res) => {
-    await Event.find({}, (err, movies) => {
+    await Event.find({}, (err, events) => {
         if (err) {
             return res.status(400).json({ success : false, error: err})
-        }
-        if (!events.length) {
-            return res
-                .status(404)
-                .json({ success : false, error : 'Event not found'})
         }
         return res.status(200).json({ success : true, data : events})
     }).catch(err => console.log(err))
 }
 
 getEventByID = async (req, res) => {
+    await Event.findOne({_id : req.params.id}, (err, event) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Event not found!'
+            })
+        }
 
+        if (!event) {
+            return res
+                    .status(404)
+                    .json({ success : false, error : "Event not found!"})
+        }
+        return res.status(200).json({ success : true, data: event})
+    }).catch(err => console.log(err))
 }
 
+getEventsByOwner = async (req, res) => {
+    await Event.find({creator : req.params.creator}, (err, events) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Event not found!'
+            })
+        }
+
+        return res.status(200).json({ success : true, data: events})
+    }).catch(err => console.log(err))
+}
+
+getEventsByVolunteer = async (req, res) => {
+    await Event.find({volunteer : req.params.volunteer}, (err, events) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Event not found!'
+            })
+        }
+
+        return res.status(200).json({ success : true, data: events})
+    }).catch(err => console.log(err))
+}
 
 module.exports = {
     createEvent,
     updateEvent,
     deleteEvent,
     getEvents,
-    getEventByID
+    getEventByID,
+    getEventsByOwner,
+    getEventsByVolunteer
 }
--- backend/routes/event-router.js
+++ backend/routes/event-router.js
@@ -2,9 +2,11 @@ var express = require('express')
 const EventCtrl = require('../controllers/event-ctrl')
 var router = express.Router()
 
-// router.post('/login', EventCtrl.login) 
 router.post('/create', EventCtrl.createEvent) 
 router.put('/update/:id', EventCtrl.updateEvent)
 router.get('/events', EventCtrl.getEvents)
+router.get('/event/:id', EventCtrl.getEventByID)
+router.get('/event/creator/:owner', EventCtrl.getEventsByOwner)
+router.get('/event/volunteer/:volunteer', EventCtrl.getEventsByVolunteer)
 
 module.exports = router
