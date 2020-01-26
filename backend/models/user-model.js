08f0f1398daef3992ce93919c59ef071d793736d
saji <saji.champlin@gmail.com>
2020-01-26 01:01:32 -0600

    fix user thingy problem bad scary

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const CoordinateSchema = require('./coordinate-model').CoordinateSchema
-const EventSchema = require('./event-model').EventSchema
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema(
@@ -9,12 +8,11 @@ const UserSchema = new Schema(
         password: {type: String, required: true},
         email: {type: String, required: true},
         location: {type: CoordinateSchema, require: true},
-        events: {type: [EventSchema]},
     },
     { timestamps : true }
 )
 
 module.exports = {
-    UserModel : mongoose.model('users', UserSchema),
+    UserModel: mongoose.model('users', UserSchema),
     UserSchema
 }
