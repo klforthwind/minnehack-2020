e16c641e92a0ce84544e396aa8739ad817a1654b
saji <saji.champlin@gmail.com>
2020-01-26 05:44:46 -0600

    event index

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,20 @@ import './index.css';
 import App from './App';
 import App2 from './App2';
 import registerServiceWorker from './registerServiceWorker';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router,Switch } from 'react-router-dom'
+import Event from './components/Event';
 
 registerServiceWorker();
 
 const routing = (
     <Router>
         <div>
-            <Route exact path="/" component={App} />
-            <Route path="/dashboard" component={App2} />
-            <Route path="/explore" component={App2} />
-            <Route path="/auth" component={App2} />
-            <Route path="/profile" component={App2} />
+                <Route exact path="/" component={App} />
+                <Route path="/dashboard" component={App2} />
+                <Route path="/explore" component={App2} />
+                <Route path="/auth" component={App2} />
+                <Route path="/profile" component={App2} />
+                <Route exact path="/event/:id" children={<Event />}/>
         </div>
     </Router>
 )
