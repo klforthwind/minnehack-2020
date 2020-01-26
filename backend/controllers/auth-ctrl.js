173b3cf60b5b39bcc8b7eec5d7a34e8dcfe471eb
saji <saji.champlin@gmail.com>
2020-01-25 21:43:42 -0600

    add auth ctrl

---
+++ backend/controllers/auth-ctrl.js
@@ -0,0 +1,28 @@
+const User = require('../models/user-model')
+
+createUser = (req, res) => {
+    if (req.)
+}
+deleteUser = (req, res) => {
+    
+}
+updateUser = (req, res) => {
+    
+}
+getUsers = (req, res) => {
+    
+}
+getUsersByID = (req, res) => {
+    
+}
+login = (req, res) => {
+    
+}
+module.exports = {
+    createUser,
+    updateUser,
+    deleteUser,
+    getUsers,
+    getUserByID,
+    login
+}
