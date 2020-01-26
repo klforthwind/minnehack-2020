52c5f0a4cfd781dc3f163b31016f89c8c7371b95
klforthwind <kl.forthwind@gmail.com>
2020-01-25 20:49:28 -0600

    add events template for now, will work on soon

---
+++ backend/controllers/event-ctrl.js
@@ -0,0 +1,13 @@
+const Event = require('../models/event-model')
+
+createEvent = (req, res) => {
+    
+}
+
+module.exports = {
+    createEvent,
+    updateEvent,
+    deleteEvent,
+    getEvents,
+    getEventByID
+}
